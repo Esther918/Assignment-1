baae8241664ebcf24b217cdb10daf2263381e76c
Esther Lin <lin1609@bu.edu>
2025-01-29 05:01:27 -0500

    Delete example_5.py

--- example_5.py
+++
@@ -1,9 +0,0 @@
-from bisection_solver import bisection_method
-# Oscillating spring
-# Formula: (k/m)^1/2 = w
-import math
-m = 2 
-k = 50
-f5 = lambda omega: omega - math.sqrt(k / m) 
-root5 = bisection_method(f5, 0, 10, tol=1e-6) 
-print(f"Angular frequency: {root5} rad/s")  # Expected answer: approximate 5
