a857a534fb452a18c820525c55fdbb14e94db524
Esther Lin <lin1609@bu.edu>
2025-02-14 15:48:46 -0500

    Update test_bisection.py

--- bisection/tests/test_bisection.py
+++ bisection/tests/test_bisection.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src'))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 from bisection_solver import bisection_method
 from bisection_solver import bisection_method as bisection_method
